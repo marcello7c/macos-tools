4ac38153085b4f64b9eaba5c7a08096551403b63
marcello7c <marcello7c@gmail.com>
2026-05-08 15:34:33 +0200

    just cleaning up

--- list-git_repos/list-git_repos.sh
+++ list-git_repos/list-git_repos.sh
@@ -1,4 +1,4 @@
-SEARCH_DIR="/Users/marcello.macnack/1-idey/2-GIT-COR/macos-tools/"
+SEARCH_DIR="/Users/marcello.macnack/1-idey/2-GIT-COR/"
 
 for dir in $(find "$SEARCH_DIR" -mindepth 1 -maxdepth 4 -type d ! -name ".git" ! -path "*/.git/*"); do
     if [ -d "$dir/.git" ]; then
