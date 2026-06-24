fb88bfa721b96d9b0622c7a0583ce6c9b959023f
marcello7c <marcello7c@gmail.com>
2026-06-24 11:09:24 +0200

    created 2 get full info scripts that pull size, files, folders -status works perfectly

---
+++ get-gdrive_size/get-gdrive-full_info2.sh
@@ -0,0 +1,9 @@
+GD="$HOME/Library/CloudStorage/GoogleDrive-USER@gmail.com/Shared drives"
+TARGET="$GD/FOLDER_NAME"
+
+find "$TARGET" -type f -print0 \
+  | xargs -0 stat -f '%z' \
+  | awk '{s+=$1} END {printf "%.2f GB (%d bytes)\n", s/1073741824, s}'
+
+echo "Files:   $(find "$TARGET" -type f | wc -l | tr -d ' ')"
+echo "Folders: $(find "$TARGET" -type d | wc -l | tr -d ' ')"
